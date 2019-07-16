ba2d264172cb459f2fbdee85ad8096945f810509
Gavin Li-Hunt <gavin@cloudone.mobi>
2019-07-16 14:34:48 +0200

    Added day click callback function and published changes

--- src/BookingCalendar.js
+++ src/BookingCalendar.js
@@ -43,7 +43,6 @@ export default class BookingCalendar extends Component {
 
   handleSelect(day) {
     if (this.props.clickable) {
-      console.log("handle select", day);
       this.props.handleDayClick(day);
       this.setState({ selected: day.date });
     }
